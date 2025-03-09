94e56fc95d8eb2ea420341720d670057d0226c99
yoryi404 <jalegrem04@iesalbarregas.es>
2025-03-09 17:31:46 +0100

    Clase 'Main' comentada con 'Javadoc'

--- src/MASTER/Main.java
+++ src/MASTER/Main.java
@@ -1,6 +1,19 @@
 package MASTER;
+/**
+ * Clase principal que contiene el método main para ejecutar el programa.
+ * Se realizan operaciones de ingreso y retiro en una cuenta bancaria.
+ * 
+ * @author Jorge Alegre
+ * @version 1.0
+ */
 public class Main {
 
+/**
+ * Método principal que ejecuta la aplicación.
+ * Crea una cuenta, muestra el saldo actual y realiza operaciones de retiro e ingreso.
+ *
+ * @param args Argumentos de línea de comandos.
+ */
     public static void main(String[] args) {
 
         CCuenta cuenta1;
@@ -26,7 +39,15 @@ public class Main {
             System.out.print("Fallo al ingresar");
         }
     }
-   
+    
+/**
+ * Método que realiza operaciones sobre una cuenta bancaria, incluyendo
+ * retiro e ingreso de dinero.
+ * 
+ * @param cuenta1  Objeto de la clase CCuenta sobre el que se opera.
+ * @param cantidad Cantidad de dinero a retirar e ingresar.
+ */
+
     public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         double saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es: " + saldoActual);
